de311335064b2c874c8ac64c602087095695993d
AlexG0311 <alexdavidgonzalez2003@gmail.com>
2025-04-22 23:51:40 -0500

    Completamos el juego del ahorcado y modificamos nombre de directorio.

--- Ahorcado/src/main/java/Arrays.java
+++ Ahorcado/src/main/java/Arrays.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public class Arrays {
     public static void main(String[] args) {
 
         // PRIMERA FORMA
--- Ahorcado/src/main/java/Juego.java
+++ Ahorcado/src/main/java/Juego.java
@@ -1,21 +1,51 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Juego {
-
-    String palabraAdivinar = "Crisitano";
-    boolean isWordAdivinada = false;
-    int intentosMaximos = 6;
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner(System.in);
     int intentos = 0;
+    int intentosMaximos = 7;
+    String palabraSecreta = "cristiano";
+    boolean isWordCorrecta = false;
 
-    char[] letrasAdivinadas = new char[palabraAdivinar.length()];
+    char[] letrasAdivinadas = new char[palabraSecreta.length()];
+
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            letrasAdivinadas[i] = '_';
+        }
+
+        while(!isWordCorrecta && intentos < intentosMaximos){
+            System.out.println("Adivina " + palabraSecreta.length() + " palabras:  " + String.valueOf(letrasAdivinadas));
+            System.out.println("Digite una letra: ");
+            char letra = teclado.next().charAt(0);
+
+            boolean letraCorrecta = false;
+
+            for(int i = 0; i < palabraSecreta.length() ; i++) {
+                if(palabraSecreta.charAt(i) == letra){
+                    letrasAdivinadas[i] = letra;
+                    letraCorrecta = true;
+                }
+            }
+            if(!letraCorrecta){
+                intentos++;
+                System.out.println("Letra incorrecta te quedan " + (intentosMaximos - intentos));
+            }
+            if(String.valueOf(isWordCorrecta).equals(letrasAdivinadas)){
+                isWordCorrecta = true;
+                System.out.println("Has ganadooo yujuuuuu");
+            }
+
+        }
+if(!isWordCorrecta){
+    System.out.println("Has perdido TONTO");
+}
+teclado.close();
 
 
 
-    Scanner teclado = new Scanner(System.in); //
 
 
-
-
-
-
+    }
 }
